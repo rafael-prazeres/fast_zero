ebac9fc5e6cbcdf2ace994768641e23d6e36c757
rafael-prazeres <rafaelprazeres10@gmail.com>
2024-12-10 06:15:19 -0300

    Aula 3 - [C]RUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,32 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_root_deve_retornar_ok_e_ola_mundo_html():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo_html(client):
     response = client.get('/ola_mundo')
 
     assert response.status_code == HTTPStatus.OK
-    assert "<h1>Olá Mundo!</h1>" in response.text
+    assert '<h1>Olá Mundo!</h1>' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'rafael.prazeres',
+            'email': 'rafael.prazeres@ufrn.br',
+            'password': '123456',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'rafael.prazeres',
+        'email': 'rafael.prazeres@ufrn.br',
+        'id': 1,
+    }
